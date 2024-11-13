af8beca7fc5105a48f955c5da1f584178a14a369
Jeffrey Hood <jeffrey.hood@gmail.com>
2024-11-13 15:27:40 -0500

    search stuff

--- static/js/search.js
+++ static/js/search.js
@@ -1,14 +1,31 @@
 
 /* Search function */
 
-function Search() {
-  var search = elasticlunr( function() ) {
-  this.addField('title');
-  this.addField('content');
-  this.addField('tags');
-  this.setRef('ref');
-  });
+async function InitSearch() {
+  const index = elasticlunr( function() {
+  this.addField('title')
+  this.addField('content')
+  this.addField('tags')
+  this.setRef('uri')
+  })
+
+  const response = await fetch("/dummy.json")
+  const results = await response.json()
+  //console.log(results)
+
+  results.forEach(doc => {
+    index.addDoc(doc)
+  })
+
+  //const tmp = index.search('Supply')
+  //console.log(tmp)
+
+  return index
 
 }
 
-
+let SearchIndex 
+(
+  async function() { 
+  SearchIndex = await InitSearch()
+})()
